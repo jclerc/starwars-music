3ca57e402e69c0510b39cf68325aa26d779fec5d
Jices <Jices@users.noreply.github.com>
2015-12-15 18:19:36 +0100

    Update hyperspace

--- src/js/app/hyperspace.js
+++ src/js/app/hyperspace.js
@@ -8,67 +8,73 @@
         var ctx = canvas.getContext('2d');
         var w = canvas.width;
         var h = canvas.height;
-        ctx.lineWidth = 2;
-        ctx.lineCap = 'round';
 
-        var spawnRadius = 300;
-        var speed = spawnRadius / 50;
+        (function loop() {
+            window.requestAnimationFrame(loop);
 
-        var init = [];
-        var maxParts = 500;
-        for (var a = 0; a < maxParts; a++) {
-            var initX = w / 2 + Math.random() * spawnRadius - spawnRadius / 2;
-            var initY = h / 2 + Math.random() * spawnRadius - spawnRadius / 2;
+            ctx.lineWidth = 2;
+            ctx.lineCap = 'round';
 
-            var r = Math.round(Math.random() * 255);
-            var g = Math.round(Math.random() * 255);
-            var b = Math.round(Math.random() * 255);
+            var spawnRadius = 300;
+            var speed = spawnRadius / 50;
 
-            init.push({
-                x: initX,
-                y: initY,
-                xs: (initX - w / 2) / speed,
-                ys: (initY - h / 2) / speed,
-                l: Math.random() * 10,
-                color: 'rgb(' + r + ',' + g + ',' + b + ')'
-            });
-        }
+            var init = [];
+            var maxParts = 500;
+            for (var a = 0; a < maxParts; a++) {
+                var initX = w / 2 + Math.random() * spawnRadius - spawnRadius / 2;
+                var initY = h / 2 + Math.random() * spawnRadius - spawnRadius / 2;
 
-        var particles = [];
-        for (var i = 0; i < maxParts; i++) {
-            particles[i] = init[i];
-        }
+                var r = Math.round(Math.random() * 255);
+                var g = Math.round(Math.random() * 255);
+                var b = Math.round(Math.random() * 255);
 
-        var draw = function () {
-            ctx.clearRect(0, 0, w, h);
-            for (var c = 0; c < particles.length; c++) {
-                var p = particles[c];
-                ctx.strokeStyle = 'rgba(255,255,255,' + p.l / 20 + ')';
-                ctx.beginPath();
-                ctx.moveTo(p.x, p.y);
-                ctx.lineTo(p.x + p.l * p.xs, p.y + p.l * p.ys);
-                ctx.stroke();
+                init.push({
+                    x: initX,
+                    y: initY,
+                    xs: (initX - w / 2) / speed,
+                    ys: (initY - h / 2) / speed,
+                    l: Math.random() * 10,
+                    color: 'rgb(' + r + ',' + g + ',' + b + ')'
+                });
             }
-            move();
-        };
 
-        var move = function () {
-            for (var j = 0; j < particles.length; j++) {
-                var p = particles[j];
-                p.x += p.xs;
-                p.y += p.ys;
+            var particles = [];
+            for (var i = 0; i < maxParts; i++) {
+                particles[i] = init[i];
+            }
 
-                // if particle position exceeds the canvas
-                if (p.x < 0 || p.y < 0 || p.x > w || p.y > h) {
-                    p.x = w / 2 + Math.random() * spawnRadius - spawnRadius / 2;
-                    p.y = h / 2 + Math.random() * spawnRadius - spawnRadius / 2;
-                    p.xs = (p.x - w / 2) / speed;
-                    p.ys = (p.y - h / 2) / speed;
+            var draw = function () {
+                ctx.clearRect(0, 0, w, h);
+                for (var c = 0; c < particles.length; c++) {
+                    var p = particles[c];
+                    ctx.strokeStyle = 'rgba(255,255,255,' + p.l / 20 + ')';
+                    ctx.beginPath();
+                    ctx.moveTo(p.x, p.y);
+                    ctx.lineTo(p.x + p.l * p.xs, p.y + p.l * p.ys);
+                    ctx.stroke();
                 }
-            }
-        };
+                move();
+            };
 
-        setInterval(draw, 30);
+            var move = function () {
+                for (var j = 0; j < particles.length; j++) {
+                    var p = particles[j];
+                    p.x += p.xs;
+                    p.y += p.ys;
+
+                    // if particle position exceeds the canvas
+                    if (p.x < 0 || p.y < 0 || p.x > w || p.y > h) {
+                        p.x = w / 2 + Math.random() * spawnRadius - spawnRadius / 2;
+                        p.y = h / 2 + Math.random() * spawnRadius - spawnRadius / 2;
+                        p.xs = (p.x - w / 2) / speed;
+                        p.ys = (p.y - h / 2) / speed;
+                    }
+                }
+            };
+
+            draw();
+
+        })();
 
     }
 
