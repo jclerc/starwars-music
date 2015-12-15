bfce817c8e6605581c61c4d0bb719c80b8e8420e
Jices <Jices@users.noreply.github.com>
2015-12-15 16:22:12 +0100

    Fix conflicts

--- build/js/script.js
+++ build/js/script.js
@@ -1,4 +1,3 @@
 "use strict";function gotoPage(o,e){var r=($("section.visible"),$("."+o));if(!r||"boolean"!=typeof e)throw new Error("Wrong arguments")}
 "use strict";console.log("Libs and App loaded !");
 //# sourceMappingURL=script.js.map
-ipt.js.map
