63fb01c6cc187e79258b6b109a8649b5bad3386a
Jices <Jices@users.noreply.github.com>
2015-12-16 17:48:33 +0100

    Finished vote page

---
+++ src/js/app/vote.js
@@ -0,0 +1,14 @@
+
+var buttons = $$('.button-vote');
+var bindVote = function (elt) {
+    elt.addEventListener('click', function (e) {
+        for (var i = buttons.length - 1; i >= 0; i--) {
+            buttons[i].innerHTML = "Je vote";
+        }
+        elt.innerHTML = "Vote validé !";
+    });
+};
+
+for (var i = buttons.length - 1; i >= 0; i--) {
+    bindVote(buttons[i]);
+}
