761152a5a0afef852e9ebe2b3cb87a98d904e20e
Jices <Jices@users.noreply.github.com>
2015-12-17 20:28:32 +0100

    Added img to loader

--- src/js/app/loader.js
+++ src/js/app/loader.js
@@ -1,17 +1,30 @@
 
 (function () {
 
-    var preloadImage = function (url) {
-        var img = new Image();
-        img.src = url;
+    var preloadImages = function (urls) {
+        for (var i = urls.length - 1; i >= 0; i--) {
+            var img = new Image();
+            img.src = urls[i];
+        }
     };
 
     App.init = function () {
 
-        var images = $$('img');
-        for (var i = images.length - 1; i >= 0; i--) {
-            preloadImage(images[i].src);
-        }
+        preloadImages([
+            'img/film-1.jpg',
+            'img/film-2.jpg',
+            'img/film-3.jpg',
+            'img/film-4.jpg',
+            'img/film-5.jpg',
+            'img/film-6.jpg',
+            'img/film-7.jpg',
+            'img/lock.svg',
+            'img/star-wars-logo.svg',
+            'img/starship-left.png',
+            'img/starship-right.png',
+            'img/vote-left.jpg',
+            'img/vote-right.jpg',
+        ]);
 
         window.addEventListener('load', function () {
             var loader = $('section.loader');
