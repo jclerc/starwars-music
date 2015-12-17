ba617dd42000daae1399be007c73f87bd6df1650
Jices <Jices@users.noreply.github.com>
2015-12-17 02:06:57 +0100

    Changed audio viz

--- src/js/app/episode1.js
+++ src/js/app/episode1.js
@@ -13,8 +13,9 @@
         width = canvas.width,
         height = canvas.height;
 
-    var frequency = 40,
-        angleCount = 30;
+    var frequency = 100,
+        amplitudeCount = 30,
+        frequencyCount = 40;
 
     // Tutorial from: http://apprentice.craic.com/tutorials/30
 
@@ -27,10 +28,14 @@
     var audioPlaying = false;
     var sampleSize = 1024;
     var amplitudeArray;
+    var frequencyArray;
     var audioUrl = 'audio/episode1.mp3';
     var middleWidth = ~~(width / 2);
     var middleHeight = ~~(height / 2);
     var lastDraw = 0;
+    var energyInterval = -1;
+    var averageVolume = 0;
+    var sinOffset = 0;
 
     var setupAudioNodes = function () {
         sourceNode     = audioContext.createBufferSource();
@@ -38,6 +43,7 @@
         javascriptNode = audioContext.createScriptProcessor(sampleSize, 1, 1);
         // Create the array for the data values
         amplitudeArray = new Uint8Array(analyserNode.frequencyBinCount);
+        frequencyArray = new Uint8Array(analyserNode.frequencyBinCount);
         // Now connect the nodes together
         sourceNode.connect(audioContext.destination);
         sourceNode.connect(analyserNode);
@@ -74,68 +80,126 @@
         console.warn(e);
     };
 
+    var getAverageVolume = function (array) {
+        var values = 0;
+        var average;
+ 
+        var length = array.length;
+ 
+        // get all the frequency amplitudes
+        for (var i = 0; i < length; i++) {
+            values += array[i];
+        }
+ 
+        average = values / length;
+        return average;
+    };
+
     context.strokeStyle = 'white';
     context.shadowBlur = 100;
     context.shadowColor = 'rgb(220, 20, 220)';
-    // context.shadowColor = 'blue';
 
-    var drawSoung = function() {
+    var drawEnergy = function () {
+        // RANDOM ELECTRIC ARC
+
+        // clearCanvas();
+        // context.lineWidth = 3;
+
+        // var k, i, value = 0;
+        // var amplitudeLength = amplitudeArray.length;
+        // var amplitudeOffset = ~~(amplitudeLength / amplitudeCount);
+        // var amplitudeDraw = ~~(width / amplitudeCount);
+        // var size = 150;
+
+        // context.beginPath();
+        // context.moveTo(0, middleHeight);
+        // k = 0;
+        // for (i = amplitudeOffset; i < amplitudeLength - amplitudeOffset; i += amplitudeOffset) {
+        //     k++;
+        //     context.lineTo(k * amplitudeDraw, (Math.random() * size - size/2) * averageVolume + middleHeight);
+
+        // }
+        // context.lineTo(width, middleHeight);
+        // context.stroke();
+
+    };
+
+    var drawSound = function () {
         if (episodeIsPlaying) {
             clearCanvas();
-            context.lineWidth = ~~(4 + Math.random() * 8);
 
             var k, i, value = 0;
-            var amplitudeLength = amplitudeArray.length;
-            var countOffset = ~~(amplitudeLength / angleCount);
-            var drawOffset = ~~(width / angleCount);
 
+            // FREQUENCY VARIABLE
+
+            var frenquencyLength = frequencyArray.length - 300;
+            var frenquencyOffset = ~~(frenquencyLength / frequencyCount);
+            var frequencyDraw = ~~(middleWidth / frequencyCount);
+
+            // DOUBLE FREQUENCY ARC - SYMMETRIC
+
+            context.lineWidth = ~~(3 + Math.random() * 6);
             context.beginPath();
             context.moveTo(0, middleHeight);
-            k = 0;
-            for (i = countOffset; i < amplitudeLength - countOffset; i += countOffset) {
-                k++;
-                value = height - (height * amplitudeArray[i] / 256) - 1;
-                context.lineTo(k * drawOffset, value);
 
+            k = 0;
+            for (i = frenquencyLength - frenquencyOffset; i >= 0; i -= frenquencyOffset) {
+                k++;
+                value = frequencyArray[i + 5] / 256;
+                context.lineTo(k * frequencyDraw, height - middleHeight - (value * middleHeight) * Math.sin(sinOffset + k / 5));
             }
+
+            k = 0;
+            for (i = 0; i < frenquencyLength - frenquencyOffset * 2; i += frenquencyOffset) {
+                k++;
+                value = frequencyArray[i + 5] / 256;
+                context.lineTo(middleWidth + k * frequencyDraw, height - middleHeight - (value * middleHeight) * Math.cos(sinOffset + k / 5));
+            }
+
             context.lineTo(width, middleHeight);
             context.stroke();
 
-            context.beginPath();
-            context.moveTo(0, middleHeight);
-            k = 0;
-            for (i = amplitudeLength - countOffset; i >= countOffset; i -= countOffset) {
-                k++;
-                value = height - (height * amplitudeArray[i] / 256) - 1;
-                context.lineTo(k * drawOffset, value);
+            sinOffset -= 0.9;
 
-            }
-            context.lineTo(width, middleHeight);
-            context.stroke();
-
-            context.beginPath();
-            context.moveTo(0, middleHeight);
-            k = 0;
-            for (i = amplitudeLength - countOffset; i >= countOffset; i -= countOffset) {
-                k++;
-                context.lineTo(k * drawOffset, Math.random() * 50 - 25 + middleHeight);
-
-            }
-            context.lineTo(width, middleHeight);
-            context.stroke();
-            
             // episodeIsPlaying = false;
-            
-            // var length = amplitudeArray.length * 0.95,
-                // start = ~~(length * 0.05);
+
+
+            // context.lineWidth = ~~(2 + Math.random() * 4);
+
+            // var amplitudeLength = amplitudeArray.length;
+            // var amplitudeOffset = ~~(amplitudeLength / amplitudeCount);
+            // var amplitudeDraw = ~~(width / amplitudeCount);
+
             // context.beginPath();
-            // var value, i, y;
             // context.moveTo(0, middleHeight);
-            // for (i = start; i < length; i += 60) {
-                // i = ~~i;
-                // value = amplitudeArray[i] / 256;
-                // y = height - (height * value) - 1;
-                // context.lineTo(i - start, y);
+            // k = 0;
+            // for (i = amplitudeOffset; i < amplitudeLength - amplitudeOffset; i += amplitudeOffset) {
+            //     k++;
+            //     value = height - (height * amplitudeArray[i] / 256) - 1;
+            //     context.lineTo(k * amplitudeDraw, value);
+
+            // }
+            // context.lineTo(width, middleHeight);
+            // context.stroke();
+
+
+            // context.beginPath();
+            // context.moveTo(0, middleHeight);
+            // k = 0;
+            // for (i = amplitudeLength - amplitudeOffset; i >= amplitudeOffset; i -= amplitudeOffset) {
+            //     k++;
+            //     value = height - (height * amplitudeArray[i] / 256) - 1;
+            //     context.lineTo(k * amplitudeDraw, value);
+            // }
+            // context.lineTo(width, middleHeight);
+            // context.stroke();
+
+            // context.beginPath();
+            // context.moveTo(0, middleHeight);
+            // k = 0;
+            // for (i = amplitudeLength - amplitudeOffset; i >= amplitudeOffset; i -= amplitudeOffset) {
+            //     k++;
+            //     context.lineTo(k * amplitudeDraw, Math.random() * 50 - 25 + middleHeight);
             // }
             // context.lineTo(width, middleHeight);
             // context.stroke();
@@ -165,12 +229,13 @@
             if (lastDraw > now - frequency) return;
             lastDraw = now;
             // get the Time Domain data for this sample
+            averageVolume = getAverageVolume(frequencyArray) / 100;
             analyserNode.getByteTimeDomainData(amplitudeArray);
-            // analyserNode.getByteFrequencyData(amplitudeArray);
+            analyserNode.getByteFrequencyData(frequencyArray);
 
             // draw the display if the audio is playing
             if (audioPlaying === true) {
-                window.requestAnimationFrame(drawSoung);
+                window.requestAnimationFrame(drawSound);
             }
         });
 
@@ -181,9 +246,12 @@
             playSound(audioData);
         }
 
+        energyInterval = setInterval(drawEnergy, 62);
+
     });
 
     App.bind('episode-1', 'unloading', function () {
+        clearInterval(energyInterval);
         episodeIsPlaying = false;
         sourceNode.stop(0);
         clearCanvas();
