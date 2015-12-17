c394d797101d04b671a8c1391afcb5d8272f8ef5
Jices <Jices@users.noreply.github.com>
2015-12-17 19:04:01 +0100

    Added loader

--- src/js/app/loader.js
+++ src/js/app/loader.js
@@ -14,6 +14,7 @@
         }
 
         window.addEventListener('load', function () {
+            App.goto('home', false);
         });
 
     };
--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,8 @@
 // If a hash is already set, go to matching page
 App.gotoHash(false);
 
+// Load the page
+App.init();
+
 // Everything is OK !
 console.log('Libs and App loaded !');
